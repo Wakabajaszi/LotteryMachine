f0357c6170626837a5a9c61911eabbea1142a393
Wakabajaszi <michal.jedrych@outlook.com>
2021-02-22 10:20:04 +0100

    added main site of LotteryMachine

---
+++ LotteryMachine/LotteryMachine/LotteryMachineMainSite.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LotteryMachine
+{
+    public partial class LotteryMachineMainForm : Form
+    {
+        public LotteryMachineMainForm()
+        {
+            InitializeComponent();
+            
+
+
+        }
+
+        private void drawingButton_Click_1(object sender, EventArgs e)
+        {
+            lotteryMachineScreenControl1.StartAnimation();
+        }
+    }
+}
