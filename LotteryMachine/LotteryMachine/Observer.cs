e5496c2aa6dcefb1225c5c6d4d2331f141a49c2f
Wakabajaszi <michal.jedrych@outlook.com>
2021-04-11 14:15:19 +0200

    Added observer class

---
+++ LotteryMachine/LotteryMachine/Observer.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LotteryMachine
+{
+    public interface IObserver
+    {
+        void Reaction();
+    }
+    class Observer
+    {
+        private List<IObserver> observers = new List<IObserver>();
+        public void AddObserver(IObserver newObserver) 
+        {
+            observers.Add(newObserver);
+        }
+        public void RemoveObserver(IObserver oldObserver) 
+        {
+            observers.Remove(oldObserver);
+            observers.TrimExcess();
+        }
+        protected void RefreshGrid() 
+        {
+            foreach (var item in observers)
+            {
+                item.Reaction();
+            }
+        }
+    }
+    class RefreshGridClass : Observer 
+    {
+        public void Refresh() 
+        {
+            base.RefreshGrid();
+        }
+    }
+}
